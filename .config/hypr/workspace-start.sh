10769081d190dbbd653548c866b38e6e2a418a6b
James Barwell <jb@jamesbarwell.co.uk>
2025-03-23 21:02:47 +0000

    Loads of hyprland improvements

--- .config/hypr/workspace-start.sh
+++ .config/hypr/workspace-start.sh
@@ -1,6 +1,8 @@
 hyprctl dispatch -- exec kitty --hold cmus
+sleep 0.2
 # For wttr.in codes, see: https://wttr.in/:help
 hyprctl dispatch -- exec kitty --hold curl --silent wttr.in/York?1nq
-hyprctl dispatch -- exec kitty --hold vim ~/docs/todos/todo
 sleep 0.2
-hyprctl dispatch resizeactive 200 0
+hyprctl dispatch -- exec kitty --hold less ~/docs/todos/todo
+sleep 0.3
+hyprctl dispatch resizeactive 200 -80
