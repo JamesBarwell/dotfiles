78ecddc1907a1076dff5f20a0e1f327374145166
James Barwell <jb@jamesbarwell.co.uk>
2025-03-28 09:10:38 +0000

    Rejig start space

--- .config/hypr/workspace-start.sh
+++ .config/hypr/workspace-start.sh
@@ -1,8 +1,7 @@
-hyprctl dispatch -- exec kitty --hold cmus
+hyprctl dispatch -- exec kitty --hold btop
 sleep 0.2
-# For wttr.in codes, see: https://wttr.in/:help
-hyprctl dispatch -- exec kitty --hold curl --silent wttr.in/York?1nq
+hyprctl dispatch -- exec kitty --hold cmus
 sleep 0.2
 hyprctl dispatch -- exec kitty --hold less ~/docs/todos/todo
 sleep 0.3
-hyprctl dispatch resizeactive 200 -80
+hyprctl dispatch resizeactive 40 140
