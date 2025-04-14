14f33e8ba98d0af0f0678e7087a02bd822a49125
James Barwell <jb@jamesbarwell.co.uk>
2025-04-14 18:59:37 +0100

    Make trackpad toggle a little more portable

--- .config/hypr/trackpad-toggle.sh
+++ .config/hypr/trackpad-toggle.sh
@@ -1,11 +1,15 @@
 status_path=$XDG_RUNTIME_DIR/trackpad-disabled
 
+# Find device with: hyprctl devices
+# device_name=synps/2-synaptics-touchpad
+device_name=synaptics-tm3512-010
+
 if [ ! -f "${status_path}" ] ; then
     notify-send -u normal "Disable Touchpad"
-    hyprctl keyword "device[synps/2-synaptics-touchpad]:enabled" false
+    hyprctl keyword "device[${device_name}]:enabled" false
     touch $status_path
 else
     notify-send -u normal "Enabling Touchpad"
-    hyprctl keyword "device[synps/2-synaptics-touchpad]:enabled" true
+    hyprctl keyword "device[${device_name}]:enabled" true
     /usr/bin/rm $status_path
 fi
