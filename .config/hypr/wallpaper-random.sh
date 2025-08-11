c871459e08db1dfa0e2bf8022e796c6fb616fd13
James Barwell <jb@jamesbarwell.co.uk>
2025-08-11 15:53:41 +0100

    Set wallpapers on different monitors at different resolutions

--- .config/hypr/wallpaper-random.sh
+++ .config/hypr/wallpaper-random.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
-WALLPAPER_DIR="$HOME/wallpapers/"
-CURRENT_WALL=$(hyprctl hyprpaper listloaded)
+MONITOR_NAME=$1
+RESOLUTION=$2
+
+WALLPAPER_DIR="$HOME/wallpapers/$RESOLUTION"
+
+#CURRENT_WALL=$(hyprctl hyprpaper listloaded)
 
 # Get a random wallpaper that is not the current one
-WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
+#PRIMARY_WALLPAPER_PATH=$(find "$PRIMARY_MONITOR_WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
+
+# Select random wallpapers for each monitor
+WALLPAPER_PATH=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
 
 # Apply the selected wallpaper
-hyprctl hyprpaper reload ,"$WALLPAPER"
+hyprctl hyprpaper reload $MONITOR_NAME,"$WALLPAPER_PATH"
