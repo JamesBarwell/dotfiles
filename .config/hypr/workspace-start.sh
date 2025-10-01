662b3c91c48e1fc50c1bec9ff93b5605508e58a4
James Barwell <jb@jamesbarwell.co.uk>
2025-10-01 20:27:05 +0100

    Swap default workspace stuff

--- .config/hypr/workspace-start.sh
+++ .config/hypr/workspace-start.sh
@@ -1,9 +1,9 @@
 hyprctl dispatch -- exec kitty --hold btop
 sleep 0.2
-hyprctl dispatch -- exec kitty --hold cmus
-sleep 0.2
 hyprctl dispatch -- exec kitty --hold vim ~/docs/todos/todo
+sleep 0.2
+hyprctl dispatch -- exec kitty --hold cmus
 sleep 0.3
-hyprctl dispatch resizeactive 40 140
+hyprctl dispatch resizeactive 40 -140
 
 #wttr.in/York?1Fn
